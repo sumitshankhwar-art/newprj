80f25a4c8a7234aa880ff5ca3268df7b51d0129a
sumit shankhwar <sumit.shankhwar@4thpointer.com>
2025-09-17 19:12:26 +0530

    just removed a console

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true}));
   await app.listen(process.env.PORT ?? 3000);
-  console.log(process.env.DBURL);
+  
 }
 bootstrap();
